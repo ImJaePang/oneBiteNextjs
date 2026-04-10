d7b2f4e57cb2f8cbbfaa0edb88ea6f7aa34bb9e8
Jaeyoung Im <narushel09@gmail.com>
2026-04-10 12:44:07 +0900

    글로벌 레이아웃 설정

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,34 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import GlobalLayout from "@/components/global-layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const onClickButton = () => {
-    router.push("/test");
-    // router.replace("/test");
-    // router.back();
-  }
-
-  useEffect(()=>{
-    router.prefetch("/test");
-  },[]);
-
   return (
-  <>
-    <header>
-      <Link href={"/"}>index</Link>
-      &nbsp;
-      <Link href={"/search"} prefetch={false}>search</Link>
-      &nbsp;
-      <Link href={"/book/1"}>book/1</Link>
-      <div>
-        <button onClick={onClickButton}>test페이지이동</button>
-      </div>
-    </header>
-    <Component {...pageProps} />
-  </>
-  )
+    <GlobalLayout >
+        <Component {...pageProps} />
+      </GlobalLayout>
+  );
 }
