a92882e3491191742ce153530eed7f4ba77a4103
Jaeyoung Im <narushel09@gmail.com>
2026-04-13 12:49:36 +0900

    layout by page, 1900

--- section02/src/components/searchable-layout.tsx
+++ section02/src/components/searchable-layout.tsx
@@ -1,10 +1,29 @@
-import { ReactNode } from "react"
+import { useRouter } from "next/router";
+import { ReactNode, useState } from "react"
 
-export default function SearchableLayout({childrn} : {
-    childrn : ReactNode
+export default function SearchableLayout({children} : {
+    children : ReactNode
 }){
+    // console.log("children : ", children);
+    const [search, setSearch] = useState("");
+    const router = useRouter();
+
+    const q = router.query.q;
+
+    const onChangeSearch = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+    }
+    
+    const onSubmit = () => {
+        if (!search) return;
+        router.push(`/search?q=${search}`);
+    }
+
     return <div>
-            <div>임시 서치바</div>
-        {childrn}
+            <div>
+                <input placeholder="place your search words" onChange={onChangeSearch} value={search}/>
+                <button onClick={onSubmit}>search</button>
+            </div>
+            <div>{children}</div>
         </div>
 }
