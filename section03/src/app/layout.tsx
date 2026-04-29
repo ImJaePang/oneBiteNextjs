2e47e8c08089549f82bd221574bee05077d20a68
ImJaePang <narushel@naver.com>
2026-04-30 07:18:07 +0900

    위치 수정

--- section03/src/app/layout.tsx
+++ section03/src/app/layout.tsx
@@ -28,12 +28,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <header>
-        <Link href={"/"}>index</Link>&nbsp;
-        <Link href={"/search"}>search</Link>&nbsp;
-        <Link href={"/book/1"}>book/1</Link>
-      </header>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <header>
+          <Link href={"/"}>index</Link>&nbsp;
+          <Link href={"/search"}>search</Link>&nbsp;
+          <Link href={"/book/1"}>book/1</Link>
+        </header>
+        {children}
+        </body>
     </html>
   );
 }
