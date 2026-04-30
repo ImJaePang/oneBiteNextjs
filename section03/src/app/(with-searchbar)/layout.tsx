466aa0d060f7bd4d0b737f8d00f799a04f091bb8
Jaeyoung Im <narushel09@gmail.com>
2026-04-30 12:47:26 +0900

    3.7) 한입북스 UI 구현하기

--- section03/src/app/(with-searchbar)/layout.tsx
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from "react";
-import SearchBar from "../../components/searchbar";
+import Searchbar from "../../components/searchbar";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <div>
-      {/* <div>임시 서치바</div> */}
-      <SearchBar />
+      <Searchbar />
       {children}
     </div>
   );
