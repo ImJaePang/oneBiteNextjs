e76c19bbcfc5cd36b895165569b9b84b01371f71
Jaeyoung Im <narushel09@gmail.com>
2026-04-30 12:21:45 +0900

    3.6) 네비게이팅

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,9 +1,18 @@
+import ClientComponent from "@/src/components/client-component";
+
 export default async function Page({
   searchParams,
-} : {
+}: {
   searchParams: Promise<{ q: string }>;
 }) {
-  const {q} = await searchParams;
-  
-  return <div>Search 페이지 : {q}</div>;
+  const { q } = await searchParams;
+
+  return (
+    <div>
+      Search 페이지 : {q}
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  );
 }
--- section03/src/components/searchbar.tsx
+++ section03/src/components/searchbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react"
 
 export default function SearchBar() {
@@ -7,9 +8,14 @@ export default function SearchBar() {
     const onChangeSearch = (e : React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
     }
+    const router = useRouter();
+
+    const onSubmit = () => {
+        router.push(`/search?q=${search}`);
+    }
 
     return <div>
         <input value={search} onChange={onChangeSearch} />
-        <button>검색</button>
+        <button onClick={onSubmit}>검색</button>
     </div>
 }
