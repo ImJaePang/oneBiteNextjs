bd55295a4a63f9d343f15fcb2da46390e44a7e5e
Jaeyoung Im <narushel09@gmail.com>
2026-05-04 12:46:21 +0900

    5.4) 라우트 세그먼트 옵션

--- section03/src/app/(with-searchbar)/page.tsx
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -3,7 +3,12 @@ import style from "./page.module.css";
 // import books from "@/mock/books.json";
 import { BookData } from "@/types";
 
-
+// export const dynamic = 'force-dynamic';
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+// 1. auto : 기본값, 아무것도 강제하지 않음
+// 2. force-dynamic :  페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static :  페이지를 강제로 Static 페이지로 설정
+// 4. error :  페이지를 강제로 Static 페이지로 설정(설정하면 안되는 이유 있으면 error)
 
 async function AllBooks (){
 
