1276d23d80cfbbd3c5c07bd6761e438fb1c3205e
Jaeyoung Im <narushel09@gmail.com>
2026-05-07 12:42:17 +0900

    7.7) 리뷰 삭제 기능 구현하기

--- section03/src/app/book/[id]/page.tsx
+++ section03/src/app/book/[id]/page.tsx
@@ -7,79 +7,75 @@ import ReviewEditor from "@/components/review-editor";
 // export const dynamicParams = false; // 다이나믹만 넣고 싶을때
 
 export function generateStaticParams() {
-    return [{ id: "1" }, { id: "2" }, { id: "3" }];
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
 
 async function BookDetail({ bookId }: { bookId: string }) {
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`,
-    );
-    if (!response.ok) {
-        if (response.status === 404) {
-            notFound();
-        }
-        return <div>오류가 발생했습니다 ...</div>;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`,
+  );
+  if (!response.ok) {
+    if (response.status === 404) {
+      notFound();
     }
-    const book = await response.json();
-    const { title, subTitle, description, author, publisher, coverImgUrl } =
-        book;
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+  const book = await response.json();
+  const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
-    return (
-        <section>
-            <div
-                className={style.cover_img_container}
-                style={{ backgroundImage: `url('${coverImgUrl}')` }}
-            >
-                <img src={coverImgUrl} />
-            </div>
-            <div className={style.title}>{title}</div>
-            <div className={style.subTitle}>{subTitle}</div>
-            <div className={style.author}>
-                {author} | {publisher}
-            </div>
-            <div className={style.description}>{description}</div>
-        </section>
-    );
+  return (
+    <section>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url('${coverImgUrl}')` }}
+      >
+        <img src={coverImgUrl} />
+      </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+      <div className={style.description}>{description}</div>
+    </section>
+  );
 }
 
-
-
 async function ReviewList({ bookId }: { bookId: string }) {
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`,
-        {
-            method: "GET",
-            next : {tags : [`review-${bookId}`]}
-        },
-    );
-
-    if (!response.ok) {
-        throw new Error(`Review fetch failed : ${response.statusText}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`,
+    {
+      next: { tags: [`review-${bookId}`] },
     }
+  );
 
-    const reviews: ReviewData[] = await response.json();
-    // console.log(reviews);
+  if (!response.ok) {
+    throw new Error(`Review fetch failed : ${response.statusText}`);
+  }
 
-    return (
-        <section>
-            {reviews.map((review) => (
-                <ReviewItem key={`review-item-${review.id}`} {...review} />
-            ))}
-        </section>
-    );
+  const reviews: ReviewData[] = await response.json();
+  // console.log(reviews);
+
+  return (
+    <section>
+      {reviews.map((review) => (
+        <ReviewItem key={`review-item-${review.id}`} {...review} />
+      ))}
+    </section>
+  );
 }
 
 export default async function Page({
-    params,
+  params,
 }: {
-    params: Promise<{ id: string }>;
+  params: Promise<{ id: string }>;
 }) {
-    const { id } = await params;
-    return (
-        <div className={style.container}>
-            <BookDetail bookId={id} />
-            <ReviewEditor bookId={id} />
-            <ReviewList bookId={id} />
-        </div>
-    );
+  const { id } = await params;
+  return (
+    <div className={style.container}>
+      <BookDetail bookId={id} />
+      <ReviewEditor bookId={id} />
+      <ReviewList bookId={id} />
+    </div>
+  );
 }
--- section03/src/components/review-item.tsx
+++ section03/src/components/review-item.tsx
@@ -1,23 +1,24 @@
 import { ReviewData } from "@/types";
 import style from "./review-item.module.css";
+import ReviewItemDeleteBtn from "./review-item-delete-btn";
 
 export default function ReviewItem({
-    id,
-    content,
-    author,
-    createdAt,
-    bookId,
+  id,
+  content,
+  author,
+  createdAt,
+  bookId,
 }: ReviewData) {
-    return (
-        <div className={style.container}>
-            <div className={style.author}>{author}</div>
-            <div className={style.content}>{content}</div>
-            <div className={style.bottom_container}>
-                <div className={style.date}>
-                    {new Date(createdAt).toLocaleString()}
-                </div>
-                <div className={style.delete_btn}>삭제하기</div>
-            </div>
+  return (
+    <div className={style.container}>
+      <div className={style.author}>{author}</div>
+      <div className={style.content}>{content}</div>
+      <div className={style.bottom_container}>
+        <div className={style.date}>{new Date(createdAt).toLocaleString()}</div>
+        <div className={style.delete_btn}>
+          <ReviewItemDeleteBtn reviewId={id} bookId={bookId} />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
